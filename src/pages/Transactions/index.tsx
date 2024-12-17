554a55cb7ebdfaf3ea1e76977994cd0bbae7df42
wilsonjp <wilsonjp.dev@outlook.com>
2024-12-17 08:48:07 -0300

    componente summary

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,7 +1,11 @@
 import { Header } from "../../components/Header"
+import { Summary } from "../../components/Summary"
 
 export function Transactions() {
     return (
-        <Header />
+        <div>
+            <Header />
+            <Summary />
+        </div>
     )
 }
