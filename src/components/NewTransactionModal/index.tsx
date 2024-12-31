faf76c5f2136bd71ecb4788b29a0a9333192859a
wilsonjp <wilsonjp.dev@outlook.com>
2024-12-31 10:25:31 -0300

    aplicando react-hook-form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,28 @@
 import * as Dialog from '@radix-ui/react-dialog'
 import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles'
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
+import * as z from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const newTransactionFormSchema = z.object({
+    description: z.string(),
+    price: z.number(),
+    category: z.string(),
+    //type: z.enum(['income', 'outcome'])
+})
+
+type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
 export function NewTransactionModal() {
+    const {register, handleSubmit, formState: { isLoading } } = useForm<NewTransactionFormInputs>({
+        resolver: zodResolver(newTransactionFormSchema)
+    })
+
+    function handleCreateNewTransaction(data: NewTransactionFormInputs) {
+        console.log(data)
+    }
+
     return (
         <Dialog.Portal>
             <Overlay />
@@ -12,10 +32,25 @@ export function NewTransactionModal() {
                     <X size={24} />
                 </CloseButton>
 
-                <form action=''>
-                    <input type="text" placeholder='Descrição' required />
-                    <input type="number" placeholder='Preço' required />
-                    <input type="text" placeholder='Categoria' required />
+                <form action='' onSubmit={handleSubmit(handleCreateNewTransaction)}>
+                    <input
+                        type="text"
+                        placeholder='Descrição'
+                        required
+                        {...register('description')}
+                    />
+                    <input
+                        type="number"
+                        placeholder='Preço'
+                        required
+                        {...register('price', { valueAsNumber: true })}
+                    />
+                    <input
+                        type="text"
+                        placeholder='Categoria'
+                        required
+                        {...register('category')}
+                    />
 
                     <TransactionType>
                         <TransactionTypeButton variant='income' value='income'>
@@ -28,7 +63,7 @@ export function NewTransactionModal() {
                         </TransactionTypeButton>
                     </TransactionType>
 
-                    <button type='submit'>
+                    <button type='submit' disabled={isLoading}>
                         Cadastrar
                     </button>
                 </form>
--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,11 +1,32 @@
 import { MagnifyingGlass } from "phosphor-react";
 import { SearchFormContainer } from "./styles";
+import { useForm } from "react-hook-form";
+import * as z from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const searchFormSchema = z.object({
+    query: z.string()
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+    const { register, handleSubmit, formState: { isSubmitting } } = useForm<SearchFormInputs>({
+        resolver: zodResolver(searchFormSchema)
+    })
+
+    function handleSearchTransaction(data: SearchFormInputs) {
+        console.log(data)
+    }
+
     return (
-        <SearchFormContainer>
-            <input type="text" placeholder="Busque por transações" />
-            <button type="submit">
+        <SearchFormContainer onSubmit={handleSubmit(handleSearchTransaction)}>
+            <input
+                type="text"
+                placeholder="Busque por transações"
+                {...register('query')}
+            />
+            <button type="submit" disabled={isSubmitting}>
                 <MagnifyingGlass size={20}/>
                 Buscar
             </button>
